cd613946c614579f99cb8e309507ecb8ff96c64d
SwithFr <contact@swith.fr>
2016-06-19 11:30:58 +0200

    use middleware

--- bin/routes/users.js
+++ bin/routes/users.js
@@ -4,7 +4,13 @@
 
 "use strict";
 
+var _middleware = require("../core/middleware");
+
+var _middleware2 = _interopRequireDefault(_middleware);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 exports.init = function (oApp) {
 
-  oApp.get('/users', require('../controllers/users/list'));
+  oApp.get('/users', _middleware2.default.isAuthenticated, require('../controllers/users/list'));
 };
