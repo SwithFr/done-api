dcc6304b4a6d2bf46bf98743acc3d896d9fd56dd
SwithFr <contact@swith.fr>
2016-06-19 16:05:06 +0200

    list all project's tasks

--- src/controllers/tasks/list.js
+++ src/controllers/tasks/list.js
@@ -4,3 +4,47 @@
  */
 
 "use strict"
+
+import middleware from "../../core/middleware"
+import zouti from 'zouti'
+import { models } from "../../core/sequelize"
+
+let json = middleware.json
+let Task = models.Task
+let User = models.User
+
+module.exports = function( oReq, oRes ) {
+    Task
+        .findAll( {
+            include: [ {
+                model: User
+            } ],
+            where: {
+                project_id: +oReq.params.projectId
+            },
+            order: 'updated_at DESC'
+        } )
+        .catch( ( oError ) => {
+            return json.error( oReq, oRes, oError, 500 )
+        } )
+        .then( ( oTasks ) => {
+            if ( !oTasks ) {
+                return json.error( oReq, oRes, {
+                    type: 'NO_TASKS_FOUND',
+                    message: 'Sorry but there are no task in this project'
+                }, 404 )
+            }
+
+            return json.send( oReq, oRes, oTasks.map( ( oTask ) => {
+                return {
+                    id: oTask.id,
+                    title: oTask.title,
+                    created_at: oTask.created_at,
+                    user: {
+                        id: oTask.User.id,
+                        login: oTask.User.login
+                    }
+                }
+            } ) )
+        } )
+}
