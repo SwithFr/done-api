f0058ad84b62d3bf54635bad2705ed0251194733
SwithFr <contact@swith.fr>
2016-06-19 14:29:11 +0200

    add task model

---
+++ src/models/task.js
@@ -0,0 +1,31 @@
+/**
+ * done-api
+ * Created by Swith on 19/06/2016.
+ */
+
+"use strict"
+
+export default function( oSequelize, DataTypes  ) {
+
+    let oColumns, oProperties
+
+    oColumns = {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            required: true
+        },
+        note: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        }
+    }
+
+    oProperties = {
+        tablename: "tasks",
+        paranoid: true,
+        underscored: true
+    }
+
+    return oSequelize.define( "Task", oColumns, oProperties )
+}
