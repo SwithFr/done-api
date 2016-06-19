b2786819a6bdb92233511c92e12eca4f84e3152a
SwithFr <contact@swith.fr>
2016-06-19 11:31:33 +0200

    db sync

--- src/core/connect.js
+++ src/core/connect.js
@@ -6,22 +6,30 @@
 
 import oSequelize from './sequelize'
 import zouti from 'zouti'
+import fs from 'fs'
 
-oSequelize.db.options.logging = false
+fs.stat( './db-sync.stub', ( oError, oStats ) => {
+    if ( oError ) {
+        oSequelize.db.options.logging = false
 
-zouti.log( "connect to database", "done:API", zouti.GREEN )
-zouti.bench( "done:db" )
-
-oSequelize.db
-    .sync( {
-        force: true
-    } )
-    .catch( function( oError ) {
-        zouti.log( oError, "done:db:connect", zouti.ERROR )
+        zouti.log( "connect to database", "done:API", zouti.GREEN )
         zouti.bench( "done:db" )
-        process.exit( 1 )
-    } )
-    .then( function() {
-        zouti.bench( "done:db" )
-        oSequelize.db.options.logging = console.log;
-    } )
+
+        oSequelize.db
+            .sync( {
+                force: true
+            } )
+            .catch( function( oError ) {
+                zouti.log( oError, "done:db:syncing", zouti.ERROR )
+                zouti.bench( "done:db" )
+                process.exit( 1 )
+            } )
+            .then( function() {
+                zouti.bench( "done:db" )
+                fs.writeFile( "./db-sync.stub", "1", {
+                    encoding: "utf-8"
+                } )
+                oSequelize.db.options.logging = console.log;
+            } )
+    }
+} )
