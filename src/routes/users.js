3111bfacbb9e4bc6a4e229686df9926f75bcef2f
SwithFr <contact@swith.fr>
2016-06-19 13:12:27 +0200

    add create route

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,10 @@
 import middleware from "../core/middleware"
 
 exports.init = function( oApp ) {
-
+    // Log user
     oApp.post( '/users/login', require( '../controllers/users/login' ) )
+
+    // Create user
+    oApp.post( '/users', require( '../controllers/users/create' ) )
 
 }
