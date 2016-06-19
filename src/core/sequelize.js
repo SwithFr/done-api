131583afa24b7f990d246ab39c56fbb534551516
SwithFr <contact@swith.fr>
2016-06-19 14:29:04 +0200

    import task model and setup relations

--- src/core/sequelize.js
+++ src/core/sequelize.js
@@ -16,9 +16,12 @@ exports.db = oSequelize = new Sequlize( "done", "root", "root", {
 exports.models = oModels = {
     User: oSequelize.import( "../models/user.js" ),
     Project: oSequelize.import( "../models/project.js" ),
-    UserProjects: oSequelize.import( "../models/user_projects.js" )
+    UserProjects: oSequelize.import( "../models/user_projects.js" ),
+    Task: oSequelize.import( "../models/task.js" )
 }
 
 // Relations
 oModels.Project.belongsTo( oModels.User )
 oModels.User.hasMany( oModels.UserProjects )
+oModels.Task.belongsTo( oModels.Project )
+oModels.Task.belongsTo( oModels.User )
