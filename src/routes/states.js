3acc9ed69cc846c9a3b55d8c4113510902092ee0
SwithFr <contact@swith.fr>
2016-06-21 16:28:29 +0200

    create states routes

---
+++ src/routes/states.js
@@ -0,0 +1,16 @@
+/**
+ * done-api
+ * Created by Swith on 21/06/2016.
+ */
+
+"use strict"
+
+import middleware from "../core/middleware"
+
+exports.init = function( oApp ) {
+    // Create state
+    oApp.post( '/states', middleware.isAuthenticated, require( '../controllers/states/create' ) )
+
+    // List all user's states
+    //oApp.get( '/states', middleware.isAuthenticated, require( '../controllers/states/list' ) )
+}
