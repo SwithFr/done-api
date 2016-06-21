f2ec77a02bdcaac3098cf249d527b97063b4f7a3
SwithFr <contact@swith.fr>
2016-06-21 14:10:07 +0200

    import tag model and init relation

--- src/core/sequelize.js
+++ src/core/sequelize.js
@@ -17,6 +17,7 @@ exports.models = oModels = {
     User: oSequelize.import( "../models/user.js" ),
     Project: oSequelize.import( "../models/project.js" ),
     UserProjects: oSequelize.import( "../models/user_projects.js" ),
+    Tag: oSequelize.import( "../models/tag.js" ),
     Task: oSequelize.import( "../models/task.js" )
 }
 
@@ -25,3 +26,4 @@ oModels.Project.belongsTo( oModels.User )
 oModels.User.hasMany( oModels.UserProjects )
 oModels.Task.belongsTo( oModels.Project )
 oModels.Task.belongsTo( oModels.User )
+oModels.Tag.belongsTo( oModels.User )
