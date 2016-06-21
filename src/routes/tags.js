9819c5eeebed627904503093ecc0ea06d8bf9eb1
SwithFr <contact@swith.fr>
2016-06-21 14:10:18 +0200

    add tags routes

---
+++ src/routes/tags.js
@@ -0,0 +1,16 @@
+/**
+ * done-api
+ * Created by Swith on 20/06/2016.
+ */
+
+"use strict"
+
+import middleware from "../core/middleware"
+
+exports.init = function( oApp ) {
+    // Create tag
+    oApp.post( '/tags', middleware.isAuthenticated, require( '../controllers/tags/create' ) )
+
+    // List all user's tags
+    oApp.get( '/tags', middleware.isAuthenticated, require( '../controllers/tags/list' ) )
+}
